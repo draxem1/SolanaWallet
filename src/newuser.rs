2ce05843f5b0799b3a500944ba9522f804bdc0db
Nick Legato <nicklegato@gmail.com>
2022-04-01 12:16:24 -0400

    Adding serialization with serde

--- src/newuser.rs
+++ src/newuser.rs
@@ -1,27 +1,28 @@
 //Renders a registration window after create new in login.rs is clicked
-
 #[path = "./pasgen.rs"]
 mod pasgen;
+
 pub mod register {
 
+use super::list;
+use serde::{Serialize, Deserialize};
 use super::pasgen::generator;
 use egui::{FontId, RichText, Color32};
+use std::fs;
+use std::fs::File;
+use std::collections::BTreeMap;
 
-	trait Validate{
-		fn data_on_file(&self) -> String;
-		fn username_exist(&self, data: &str) -> bool;
-		fn valid_phone(&self, phone: &str) -> bool;
-		fn email(&self, email: &str) -> bool;
-		fn password_check(&self, password: &str) -> bool;
-	}
-
-	#[derive(Default)]
+	#[derive(Serialize, Deserialize, Debug, Default)]
 	pub struct User{
 		first_name: String,
 		last_name: String,
 		email: String,
+
+		#[serde(skip_serializing, skip_deserializing)]
 		username: String,
 		password: String,
+
+		#[serde(skip_serializing, skip_deserializing)]
 		check: String,
 		phone: String,
 	}
@@ -115,8 +116,7 @@ use egui::{FontId, RichText, Color32};
                 ui.add_space(220.0);
           
                 if ui.button("Submit").clicked() {
-                	let valid = self.data_on_file();
-                	println!("{}",valid);
+                	self.save();
                 }
             });
             self.first_name = first;
@@ -127,41 +127,129 @@ use egui::{FontId, RichText, Color32};
             self.password = pass;
             self.check = ch;
 		}
+
+	fn save(&self){
+
+		let list;
+		loop{
+			list = match list::open_file("list.txt") {
+			Ok(s) => String::from(s),
+			Err(_) => {File::create("list.txt").expect("Failed to create file!!!");
+						fs::write("list.txt", b"none").expect("Failed to write none to new file!!");
+						continue},	
+			};
+			break;
+		}
+
+		let valid_list: BTreeMap<String, String>;
+		loop {
+				valid_list = match list::get_list(&list) {
+				Ok(l) => l,
+				Err(_) => {list::default(); continue},
+			};
+			break;
+		}
+
+		let valid_user = list::search(valid_list, &self.username);
+
+		if valid_user {
+			println!("An account under this user name already exist!!");
+		}
+		else {
+			
+	    }
+	}
+}}
+
+pub mod list {
+
+use serde::{Serialize, Deserialize};
+pub use super::register::User;
+use std::collections::BTreeMap;
+use std::collections::BTreeMap as Map;
+use std::io::Read;
+use std::fs::File;
+use std::fs;
+use std::error::Error;
+
+		#[derive(Serialize, Deserialize, Debug, Default)]
+		struct Copy{
+			first_name: String,
+			last_name: String,
+			email: String,
+
+			#[serde(skip_serializing, skip_deserializing)]
+			username: String,
+			password: String,
+
+			#[serde(skip_serializing, skip_deserializing)]
+			check: String,
+			phone: String,
 	}
 
-	impl Validate for User{
-	 	fn data_on_file(&self) -> String {
-	 		use std::io::Read;
-			use std::fs::File;
-			use std::error::Error;
-			//use std::fs;
+	impl Copy {
+		fn new() -> Self {
+			Self {
+				first_name: String::new(),
+				last_name: String::new(),
+				email: String::new(),
+				username: String::new(),
+				password: String::new(),
+				check: String::new(),
+				phone: String::new(),
+			}
+		}
+	}
+	pub fn open_file(file: &str) -> Result<String, Box<dyn Error>> {
+	    let mut file = File::open(file)?;
+	    let mut contents = String::new();
 
-	 		let openfile = |file: &str| -> Result<String, Box<dyn Error>> {
-	 			let mut file = File::open(file)?;
-	    		let mut contents = String::new();
+	    file.read_to_string(&mut contents).expect("No Value to Return!!!");
 
-	    		file.read_to_string(&mut contents).expect("No Value to Return!!!");
+	    Ok(contents)
+	}
+    
+    pub fn search(list: BTreeMap<String, String>, username: &str) -> bool{
 
-	    		Ok(contents)
-	 		};
+		let result: bool = match list.get(username) {
+			Some(_u) => true,
+			_ => false,
+		};
+    	result
+    }
 
-	 		let data = match openfile("data.txt") {
-	 			Ok(s) => s,
-	 			Err(_) => panic!("Cannot access Data!!"),
-	 		};
+	pub fn get_list(list: &str) -> Result<BTreeMap<String, String>, Box<dyn Error>> {
+		let test: BTreeMap<String, String> = serde_json::from_str(&list)?;
 
-	 		let user_exist = self.username_exist(&data);
-	 		println!("{}", user_exist);
+		Ok(test)
+	}
 
-	 		data
-	 	}
-  		fn username_exist(&self, data: &str) -> bool {
-  			let jason = format!("{} {{",&self.username );
-  			let valid = data.contains(&jason);
-  			valid
-  		}
-  		fn valid_phone(&self, _: &str) -> bool { todo!() }
-  		fn email(&self, _: &str) -> bool { todo!() }
-  		fn password_check(&self, _: &str) -> bool { todo!() }
- 	}
+	pub fn get_user(list: BTreeMap<String, String>, username: &str) -> User {
+
+		let result: User = match list.get(username) {
+			Some(u) => serde_json::from_str(&u).unwrap(),
+			_ => panic!("user exist but can't get info!!!"),
+		};
+
+    	result
+	}
+
+
+	pub fn add_user(mut list: BTreeMap<String, String>, username: &str, data: &User){
+		let serialized = serde_json::to_string(&data).unwrap();
+		list.insert(username.to_string(), serialized);
+		let json = serde_json::to_string(&list).unwrap();
+
+		fs::write("list.txt", json).expect("Failed to add to the list!!");
+	}
+	pub fn default(){
+		let user = Copy::new();
+
+		let mut metadata = Map::new();
+		let serialized = serde_json::to_string(&user).unwrap();
+		metadata.insert(&user.username, serialized);
+		let json = serde_json::to_string(&metadata).unwrap();
+
+		fs::write("list.txt", json).expect("Failed to write none to new file!!");
+	}
 }
