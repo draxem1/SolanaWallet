f3c9685c962760317981abe16f75bfc4036ea27b
Nick Legato <nicklegato@gmail.com>
2022-04-06 09:06:17 -0400

    Basic look for wallet until hooked to Sol network

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,10 +1,31 @@
 pub mod wall{
-use egui;
+use egui::{RichText, Color32};
+
+	#[derive(Debug)]
+	enum Amount{
+		Sol,
+	}
+
+	fn get(amount: Amount) -> f32{
+		match amount {
+			Amount::Sol => 0.0,
+		}
+	}
 
 	pub fn wallet_screen(ui: &mut egui::Ui) {
 
 		egui::Grid::new("Wallet").show(ui, |ui| {
-			ui.label("This is the wallet");
+			ui.label(RichText::new("Sol....:").size(40.0).color(Color32::from_rgb(56,250,82)));
+			ui.label(RichText::new(format!("{}", get(Amount::Sol))).size(40.0));
+			ui.end_row();
+
+			if ui.button("Send").clicked() {
+
+			}
+
+			if ui.button("Recieve").clicked() {
+
+			}
 		});
 	}
 }
